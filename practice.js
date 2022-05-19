3b8c7588b2880dd66fe1e3d83ba6cdad0cc6e3a4
KacyClark <kacy@usa.com>
2022-05-18 23:22:58 -0400

    js to create simple receipt for purchase of item to include tax
    JS to create simple receipt for purchase of an item to include tax

---
+++ practice.js
@@ -0,0 +1,12 @@
+let userName = prompt("Enter your name: ");
+let email = prompt("Enter your email address: ");
+let salary = prompt("Enter your monthly salary");
+let annualSalary = monthlySalary * 12;
+document.write(`
+<div>
+<p>Name: ${userName}</p> 
+<p>Email: ${email}</p> 
+<p>Salary: ${annual salary</p>
+</div>
+`);
+
---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Script");
+prompt("Enter desired product");
+prompt("Enter quantity wanted");
+prompt("Enter product price");
+
+let priceCalc = {
+    price * quantity;
+}    
+let tax = priceCalc * .06;
+let total = {
+    (priceCalc + tax);
+}    
+/* display info*/
+document.write(
+    <ol>
+      <li>product</li>;
+      <li>quantity</li>;
+      <li>priceCalc</li>;
+      <li>tax</li>;
+      <li>total</li>;
+    </ol>
+);
